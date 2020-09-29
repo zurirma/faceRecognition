d8e759e8c612c13ecec9ac3a9092a6bb71a82232
zurirma2020 <58671073+zurirma2020@users.noreply.github.com>
2020-09-29 07:41:27 -0300

    Components Builded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import Navigation from './Component/Navigation/Navigation';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
-
-function App() {
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+class App extends Component {
+  render() {
   return (
     <div className="App">
+    <Particles 
+    params={particlesOptions}
+  />
       <Navigation />
-      {/*<Logo />
+      <Logo />
       <ImageLinkForm />
-      <FaceREcognition />*/}
+      <Rank />
+      {/*<FaceRecognition />*/}
     </div>
   );
+}
 }
 
 export default App;
