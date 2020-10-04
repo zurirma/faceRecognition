4eda35ecd118c95bfbdf670370e3fd1d0b7c6931
zurirma <rodriguezzurirma@gmail.com>
2020-10-04 14:51:17 -0300

    Background fixed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import './App.css';
+
+
 const particlesOptions = {
   particles: {
     line_linked: {
@@ -20,13 +22,13 @@ class App extends Component {
   render() {
   return (
     <div className="App">
-    <Particles 
+    <Particles className='particles'
     params={particlesOptions}
   />
       <Navigation />
       <Logo />
-      <ImageLinkForm />
       <Rank />
+      <ImageLinkForm />
       {/*<FaceRecognition />*/}
     </div>
   );
